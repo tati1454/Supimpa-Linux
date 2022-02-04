6408dd6085686c9a96070d26d0e4b3fd84009053
Tati <tatidev1454@protonmail.com>
2022-02-04 15:40:43 -0300

    Check the existence of the Content-Length header

--- utils.py
+++ utils.py
@@ -15,13 +15,19 @@ def download_file(url, output):
         return
 
     with requests.get(url, stream=True) as r:
-        total_size = int(r.headers["Content-Length"])
+        total_size = 0
+        if "Content-Lenght" in r.headers:
+            total_size = int(r.headers["Content-Length"])
+
         with open(output, "wb") as f:
             progress = 0
             for c in r.iter_content(chunk_size=8192):
                 progress += 8192
                 f.write(c)
-                print(f"\rDownloading: {url} to {output} [{(progress / total_size) * 100:.2f}/100]", end="")
+                if total_size != 0:
+                    print(f"\rDownloading: {url} to {output} [{(progress / total_size) * 100:.2f}/100]", end="")
+                else:
+                    print(f"\rDownloading: {url} to {output}", end="")
 
             print("")
 
